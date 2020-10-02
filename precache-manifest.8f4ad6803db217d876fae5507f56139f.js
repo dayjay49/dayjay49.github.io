80c0710864e30417db21cb68ba13cfbfee7115a4
David <dayjaycs49@gmail.com>
2020-10-02 15:36:44 -0400

    Updates

---
+++ precache-manifest.8f4ad6803db217d876fae5507f56139f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd10594d244f1358fbb323db667034f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91700abe45e3d8231dc4",
+    "url": "/static/css/2.5b44f0a0.chunk.css"
+  },
+  {
+    "revision": "23fafe889c72fc9aa309",
+    "url": "/static/css/main.9fc689ea.chunk.css"
+  },
+  {
+    "revision": "91700abe45e3d8231dc4",
+    "url": "/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23fafe889c72fc9aa309",
+    "url": "/static/js/main.0845c030.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "d24d6e2d98c626b5c0d7c5cddfac8797",
+    "url": "/static/media/Jae Hui Seo Resume 2020.d24d6e2d.pdf"
+  }
+]);
