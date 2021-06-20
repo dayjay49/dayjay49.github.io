571e54661a29b01a5037ae0df3b8426934a44ad4
David <dayjaycs49@gmail.com>
2021-06-20 12:16:41 -0400

    Updates

---
+++ precache-manifest.0594444f490f40d54ed284b8519f5ec1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06feeca2ce05586209562914fc1b3885",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7faf711dd0b8e8a4cba4",
+    "url": "/static/css/2.5b44f0a0.chunk.css"
+  },
+  {
+    "revision": "e143cf3d92482259d0b7",
+    "url": "/static/css/main.9fc689ea.chunk.css"
+  },
+  {
+    "revision": "7faf711dd0b8e8a4cba4",
+    "url": "/static/js/2.63a94af8.chunk.js"
+  },
+  {
+    "revision": "0001512ff5376eafd7f76369861b7a6e",
+    "url": "/static/js/2.63a94af8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e143cf3d92482259d0b7",
+    "url": "/static/js/main.4e6f06cf.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "df159a32bf2c9ddb05f7d25234172b16",
+    "url": "/static/media/Jae Hui Seo_Resume_2021.df159a32.pdf"
+  }
+]);
