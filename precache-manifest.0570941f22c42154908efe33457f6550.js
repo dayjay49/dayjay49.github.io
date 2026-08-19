7d8657cae60f9adbc516519ed5da3b523d7921a3
Jae Hui Seo <dayjaycs49@gmail.com>
2026-08-19 16:47:28 -0400

    Updated resume for 2026

--- precache-manifest.0570941f22c42154908efe33457f6550.js
+++ precache-manifest.0570941f22c42154908efe33457f6550.js
@@ -1,34 +1,34 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dec361dc2b1ef675a72421938408d6aa",
-    "url": "/index.html"
+    revision: "dec361dc2b1ef675a72421938408d6aa",
+    url: "/index.html",
   },
   {
-    "revision": "6378587a02edc1acd3cd",
-    "url": "/static/css/2.c1fb602b.chunk.css"
+    revision: "6378587a02edc1acd3cd",
+    url: "/static/css/2.c1fb602b.chunk.css",
   },
   {
-    "revision": "0033ba374661c2551f23",
-    "url": "/static/css/main.75b0ff9c.chunk.css"
+    revision: "0033ba374661c2551f23",
+    url: "/static/css/main.75b0ff9c.chunk.css",
   },
   {
-    "revision": "6378587a02edc1acd3cd",
-    "url": "/static/js/2.63a94af8.chunk.js"
+    revision: "6378587a02edc1acd3cd",
+    url: "/static/js/2.63a94af8.chunk.js",
   },
   {
-    "revision": "0001512ff5376eafd7f76369861b7a6e",
-    "url": "/static/js/2.63a94af8.chunk.js.LICENSE.txt"
+    revision: "0001512ff5376eafd7f76369861b7a6e",
+    url: "/static/js/2.63a94af8.chunk.js.LICENSE.txt",
   },
   {
-    "revision": "0033ba374661c2551f23",
-    "url": "/static/js/main.091bd499.chunk.js"
+    revision: "0033ba374661c2551f23",
+    url: "/static/js/main.091bd499.chunk.js",
   },
   {
-    "revision": "c88671eb88bf6c28dee9",
-    "url": "/static/js/runtime-main.6a060e8a.js"
+    revision: "c88671eb88bf6c28dee9",
+    url: "/static/js/runtime-main.6a060e8a.js",
   },
   {
-    "revision": "a692e570cd5110b0892d0e092a5192ef",
-    "url": "/static/media/Jae Hui Seo_Resume_2023.a692e570.pdf"
-  }
+    revision: "a692e570cd5110b0892d0e092a5192ef",
+    url: "/static/media/Jae Hui Seo_Resume_2026.pdf",
+  },
 ]);
